d718f8cbd02a456a66a4de13d573e54674b1fa5a
Hugo Nguyen <hugo@scribd.com>
2016-02-03 19:23:47 -0800

    milestone 2

--- instagram-codepath/PhotosViewController.swift
+++ instagram-codepath/PhotosViewController.swift
@@ -7,8 +7,34 @@
 //
 
 import UIKit
+import AFNetworking
 
 class ViewController: UIViewController {
+    var images: [NSDictionary]?
+    
+    func fetch() {
+        let clientId = "e05c462ebd86446ea48a5af73769b602"
+        let url = NSURL(string:"https://api.instagram.com/v1/media/popular?client_id=\(clientId)")
+        let request = NSURLRequest(URL: url!)
+        let session = NSURLSession(
+            configuration: NSURLSessionConfiguration.defaultSessionConfiguration(),
+            delegate:nil,
+            delegateQueue:NSOperationQueue.mainQueue()
+        )
+        
+        let task : NSURLSessionDataTask = session.dataTaskWithRequest(request,
+            completionHandler: { (dataOrNil, response, error) in
+                if let data = dataOrNil {
+                    if let responseDictionary = try! NSJSONSerialization.JSONObjectWithData(
+                        data, options:[]) as? NSDictionary {
+                            NSLog("response: \(responseDictionary)")
+                            self.images = responseDictionary["data"] as! [NSDictionary]
+                            print(self.images)
+                    }
+                }
+        });
+        task.resume()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
